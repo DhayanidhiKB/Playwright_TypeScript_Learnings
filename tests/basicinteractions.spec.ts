a04296c8a8e8b6d51db7eab6ccf40de40f83caf5
Dhayanidhi KB <dayanidhipraveen44@gmail.com>
2025-12-11 22:40:28 +0530

    parallelExecution

--- tests/basicinteractions.spec.ts
+++ tests/basicinteractions.spec.ts
@@ -1,14 +1,16 @@
 import { expect, test } from "@playwright/test";
 
-// test('basic interactions (spec)', async ({ page }) => {
-//     await page.goto("https://lambdatest.com/selenium-playground/simple-form-demo", { waitUntil: 'domcontentloaded', timeout: 60000 });
-//     const messageInput=page.locator("//input[@id='user-message']");
-//     await messageInput.scrollIntoViewIfNeeded();
-//     await expect(messageInput).toHaveAttribute('placeholder','Please enter your Message');
-//     console.log("Before Entering data :" + await messageInput.inputValue());
-//     await messageInput.fill("Hello, This is a test message!");
-//     console.log("After Entering data :" + await messageInput.inputValue());
-// });
+test.describe.configure({ mode: 'parallel' });
+
+test("basicinteractions", async ({ page }) => {
+    await page.goto("https://lambdatest.com/selenium-playground/simple-form-demo", { waitUntil: 'domcontentloaded', timeout: 60000 });
+    const messageInput=page.locator("//input[@id='user-message']");
+    await messageInput.scrollIntoViewIfNeeded();
+    await expect(messageInput).toHaveAttribute('placeholder','Please enter your Message');
+    console.log("Before Entering data :" + await messageInput.inputValue());
+    await messageInput.fill("Hello, This is a test message!");
+    console.log("After Entering data :" + await messageInput.inputValue());
+})
 
 test("sum",async({page})=>{
     await page.goto("https://lambdatest.com/selenium-playground/simple-form-demo", { waitUntil: 'domcontentloaded', timeout: 60000 });
@@ -24,14 +26,14 @@ test("sum",async({page})=>{
     await page.waitForTimeout(500);
     const resultText = page.locator("//div[@id='user-message']//p[@id='addmessage']");
     await expect(resultText).toHaveText("30", { timeout: 10000 });
-});
+})
 
-// test("checkbox demo",async({page})=>{
-//     await page.goto("https://lambdatest.com/selenium-playground/checkbox-demo", { waitUntil: 'domcontentloaded', timeout: 60000 });
-//     const singleCheckbox=page.locator("//input[@id='isAgeSelected']");
-//     await singleCheckbox.scrollIntoViewIfNeeded();
-//     await singleCheckbox.check();
-//     const successMessage=page.locator("//div[@id='txtAge']");
-//     await expect(successMessage).toHaveText("Success - Check box is checked");
-// });
+test("checkbox demo",async({page})=>{
+    await page.goto("https://lambdatest.com/selenium-playground/checkbox-demo", { waitUntil: 'domcontentloaded', timeout: 60000 });
+    const singleCheckbox=page.locator("//input[@id='isAgeSelected']");
+    await singleCheckbox.scrollIntoViewIfNeeded();
+    await singleCheckbox.check();
+    const successMessage=page.locator("//div[@id='txtAge']");
+    await expect(successMessage).toHaveText("Success - Check box is checked");
+})
 
