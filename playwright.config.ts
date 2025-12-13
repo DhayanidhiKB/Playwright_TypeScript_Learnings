e6f84158cecf27b246d625e5d2aa16e32c258c5f
Dhayanidhi KB <dayanidhipraveen44@gmail.com>
2025-12-14 01:00:56 +0530

    Cross Browser Testing in Config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,24 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig, devices} from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-  //testMatch: '**/handlingdropdowns.test.ts', // <-- glob works for all subfolders
+  testMatch: '**/handlingdropdowns.test.ts', // <-- glob works for all subfolders
+  projects: [
+    {
+      name: 'Chrome',
+      use: {
+          ...devices['Desktop Chrome'],
+          channel: 'chrome',
+      }
+    },
+    {
+      name: 'Edge',
+      use: {
+        ...devices['Desktop Edge'],
+        channel: 'msedge',
+      }
+    },
+  ],
   use: {
     headless: false,
     screenshot: 'off',
